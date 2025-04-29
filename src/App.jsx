d1d97fd91c6da0962d069b58e75174ff39c5094f
Pfeiffer <franz.pfeiffer@dxc.com>
2025-04-29 11:48:52 -0600

    Header y tarjeta componentes completos en funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,62 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
 import Tarjeta from './Tarjeta'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+  const personajes = [
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeRaxgqEVBX4jF4Ul3jzOJFElZY8eI6A57QA&s",
+      name: "Superman",
+      universe: "DC",
+      description: "El más fuerte de todos los superhéroes",
+      powers: "Rayos X, volar, super fuerza",
+      stats: [
+        { key: 'Fuerza', value: 50 },
+        { key: 'Velocidad', value: 100 },
+        { key: 'Defensa', value: 400 }
+      ]
+    },
+    {
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Batman_Detective_Comics_1000_Variant.jpg/250px-Batman_Detective_Comics_1000_Variant.jpg",
+      name: "Batman",
+      universe: "DC",
+      description: "Detective experto y millonario",
+      powers: "Inteligencia, gadgets, artes marciales",
+      stats: [
+        { key: 'Fuerza', value: 20 },
+        { key: 'Velocidad', value: 30 },
+        { key: 'Defensa', value: 70 }
+      ]
+    },
+    {
+      image: "https://m.media-amazon.com/images/M/MV5BYWRkN2M1NmQtZjhjMi00ODFjLTk2ODctYWJiOWNkNjg1OGY4XkEyXkFqcGc@._V1_.jpg",
+      name: "Spiderman",
+      universe: "Marvel",
+      description: "Obtuvo sus poderes de la picadura de una araña",
+      powers: "Tira telaraña, trepa paredes, se culumpia entre edificios", 
+      stats: [
+        { key: 'fuerza', value: 20 }, 
+        { key: 'velocidad', value: 90 }, 
+        { key: 'defensa', value: 100 }
+        ]
+    }
+  ];
 
   return (
     <>
-      <header>
-        <Header/>
-      </header>
-      <main>
-        <Tarjeta image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeRaxgqEVBX4jF4Ul3jzOJFElZY8eI6A57QA&s" name="superman" universe="DC" description="El más fuerte de todos los superhéroes" powers="Rayos X, volar, super fuerza" stats={[{ key: 'fuerza', value: 50 }, { key: 'velocidad', value: 100 }, { key: 'defensa', value: 400 }]}/>
-      </main>
+      <Header/>
+      <Container>
+        <Row>
+          {personajes.map((personaje, index) => (
+            <Col md={4} key={index}> {/* 3 tarjetas por fila */}
+              <Tarjeta {...personaje} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
 export default App
--- src/Tarjeta.jsx
+++ src/Tarjeta.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./Tarjeta.css";
 
 function Tarjeta(props){
     return(
         <Card style={{ width: '18rem', margin: '2rem'}}>
             <Card.Img variant="top" src={props.image} alt={props.name} />
-            <h3>{props.name}</h3>
-            <h2>{props.universe}</h2>
+            <h1>{props.name}</h1>
+            <h3>{props.universe}</h3>
             <p>{props.description}</p>
             <p>{props.powers}</p>
             <ListGroup>
